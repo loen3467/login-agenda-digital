255b3fd0e07cd20e840b1c00e28d8df72b6e8c3d
mario <mariomachaca5477@gmail.com>
2024-06-08 17:58:04 -0400

    Login responsivo

--- src/components/login/LoginContainer.jsx
+++ src/components/login/LoginContainer.jsx
@@ -143,7 +143,7 @@ const Container = styled.div`
     overflow: hidden;
     width: 800px;
     max-width: 100%;
-    min-height: 680px;
+    min-height: 650px;
   }
 
   .form-container {
@@ -285,149 +285,14 @@ const Container = styled.div`
       }
     }
   }
+  @media (max-width: 1500px) {
+    .container {
+      min-height: 550px;
+      .title {
+        font-weight: bold;
+        margin: 0;
+        font-size: 2rem;
+      }
+    }
+  }
 `;
-
-{
-  /* <Container>
-      <section className="imgseccion">
-        <h1>BIENVENIDOS</h1>
-        <h1>AGENDA DIGITAL</h1>
-        <img src={logoAgendaDigital} alt="" />
-        <div className="fondocontent">
-          <img src={astronauta} />
-        </div>
-        <h4>Hola, inicia sesión con tu cuenta o crea una si no tienes.</h4>
-      </section>
-      <section className="panelsesion">
-        <LoginEmailPassword />
-      </section>
-    </Container> */
-}
-
-// const Container = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   min-height: 100vh;
-//   padding: 30px;
-//   background: radial-gradient(#b5ffff, #239089);
-//   flex-direction: column-reverse;
-//   width: 100%;
-
-//   .imgseccion {
-//     background: radial-gradient(#b5ffff, #239089);
-
-//     border-radius: 15px;
-//     padding: 20px;
-
-//     height: 100%;
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-//     flex-direction: column;
-//     text-align: center;
-//     gap: 35px;
-//     margin-top: 20px;
-//     box-shadow: 0px 10px 20px 0px rgba(0, 0, 0, 0.12);
-//     h1 {
-//  font-size: 35px;
-//  font-weight: 650;
-//  color: #ffffff;
-// //     }
-// //     .fondocontent {
-//  display: flex;
-//  justify-content: center;
-//  img {
-//    width: 50%;
-//    -webkit-animation: flotar 3s ease-in-out infinite;
-//    animation: flotar 3s ease-in-out infinite;
-//  }
-// //     }
-// //   }
-
-// //   .panelsesion {
-// //     display: flex;
-// //     flex-direction: column;
-// //     gap: 40px;
-// //     height: 100%;
-// //     display: flex;
-// //     justify-content: center;
-// //     align-items: center;
-// //     h2 {
-//  color: white;
-//  text-align: center;
-//  font-weight: 600;
-//  font-size: 2.5rem;
-// //     }
-// //     .btniniciar {
-//  display: flex;
-//  align-items: center;
-//  gap: 12px;
-//  border-style: none;
-
-//  img {
-//    width: 30px;
-//  }
-//  background-color: white;
-
-//  padding: 15px 30px;
-//  border-radius: 50px;
-//  font-weight: 400;
-//  font-size: 0.8rem;
-
-//  transition: all 0.25s ease;
-//  box-shadow: 0px 10px 20px 2px rgba(0, 0, 0, 0.12);
-
-//  &:hover {
-//    transform: translateY(-5px);
-//    box-shadow: 0px 20px 40px 0px rgb(0 0 0 / 10%);
-//    cursor: pointer;
-//  }
-// //     }
-// //     .social {
-//  gap: 20px;
-//  display: flex;
-//  justify-content: center;
-//  align-content: space-between;
-//  color: white;
-//  font-size: 30px;
-//  position: relative;
-//  bottom: 0;
-
-//  .icons:hover {
-//    transform: translateY(10px);
-//    transition: all 0.5s;
-//  }
-// //     }
-// //   }
-// //   @media (min-width: 64em) {
-// //     flex-direction: row;
-// //     .imgseccion {
-//  margin-top: 0;
-//  width: 50%;
-// //     }
-// //     .panelsesion {
-//  width: 50%;
-// //     }
-// //   }
-// //   @media (max-width: 48em) {
-// //     .imgseccion {
-//  .fondocontent {
-//    img {
-//      /* width: 80%; */
-//    }
-//  }
-// //     }
-// //   }
-// //   @keyframes flotar {
-// //     0% {
-//  transform: translate(0, 0);
-// //     }
-// //     50% {
-//  transform: translate(0, 20px);
-// //     }
-// //     100% {
-//  transform: translate(0, 0px);
-// //     }
-// //   }
-// // `;
